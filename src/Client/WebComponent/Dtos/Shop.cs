87db27cf447e9bccd16c9c572cbd5e227712e9f2
Hana Hardy <137615711+HanaHTaeeb@users.noreply.github.com>
2025-01-08 22:51:44 +0300

    Bug fix

--- src/Client/WebComponent/Dtos/Shop.cs
+++ src/Client/WebComponent/Dtos/Shop.cs
@@ -8,8 +8,7 @@ namespace WebComponent.Dtos
 {
     public record Shop (int Id, string Name,string Address, string City, string ContactNo, string OwnerName)
     {
-        public decimal Rate { get; init; } = 3.5M;
-        public string ImageUrl { get; set; }
+        public string ImageUrl { get; set; } = "assets/Image/avita.svg";
     }
 }
 //public string ImageUrl { get; set; } = "assets/Image/avita.svg";
