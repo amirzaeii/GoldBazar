e87623f487daeeee6d704697d48cd6626f04cc18
Abbas Mirzaei <Abbas.mi14@gmail.com>
2025-01-30 09:19:44 +0300

    fix images and icons

--- src/Client/WebComponent/Dtos/Shop.cs
+++ src/Client/WebComponent/Dtos/Shop.cs
@@ -2,8 +2,8 @@
 {
     public record Shop (int Id, string Name,string Address, string City, string ContactNo, string OwnerName)
     {
-        public string ImageUrl { get; set; } = "assets/Image/avita.svg";
+        public string ImageUrl { get; set; } = "_content/WebComponent/assets/Image/avita.svg";
     }
 }
-//public string ImageUrl { get; set; } = "assets/Image/avita.svg";
+//public string ImageUrl { get; set; } = "_content/WebComponent/assets/Image/avita.svg";
 //public int ReviewCount { get; set; } = 57;
