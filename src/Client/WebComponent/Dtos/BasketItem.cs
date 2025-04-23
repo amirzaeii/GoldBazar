e6b46ec7bda44b2db8be556cc9b924c6eb65aeed
Hana Hardy <137615711+HanaHTaeeb@users.noreply.github.com>
2025-04-24 00:40:58 +0300

    Fixed cart page.

--- src/Client/WebComponent/Dtos/BasketItem.cs
+++ src/Client/WebComponent/Dtos/BasketItem.cs
@@ -1,3 +1,5 @@
+using WebComponent.Services;
+
 namespace WebComponent.Dtos;
 public class BasketItem
 {
@@ -6,5 +8,21 @@ public class BasketItem
     public required string ProductName { get; set; }
     public decimal UnitPrice { get; set; }
     public decimal OldUnitPrice { get; set; }
-    public int Quantity { get; set; }
+    //public int Quantity { get; set; }'
+    public Action<BasketItem, int>? OnQuantityChanged { get; set; }
+
+    private int _quantity;
+    public int Quantity
+    {
+        get => _quantity;
+        set
+        {
+            if (_quantity == value) return;
+            _quantity = value;
+            // fire the page’s handler
+            OnQuantityChanged?.Invoke(this, value);
+        }
+    }
+    public string StoreName { get; set; }
+
 }
