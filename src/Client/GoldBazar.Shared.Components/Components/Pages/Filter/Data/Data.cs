18b0ca9f47057ec2b2ebe55bf141391dfa734e34
Hana Hardy <137615711+HanaHTaeeb@users.noreply.github.com>
2025-07-10 13:06:14 +0300

    Added Admin login page. Transfered all components to Shared.GoldBazar.

---
+++ src/Client/GoldBazar.Shared.Components/Components/Pages/Filter/Data/Data.cs
@@ -0,0 +1,226 @@
+
+
+using GoldBazar.Shared.DTOs;
+
+public static class DummySearchData
+{
+    public static SearchResultDTO GetSearchResult()
+    {
+        return new SearchResultDTO
+        {
+            Sorts = new List<Sort>
+            {
+                new Sort
+                {
+                    Title = "هەرزان بۆ گران",
+                    Value = "ascending"
+                },
+                new Sort
+                {
+                    Title = "گران بۆ هەرزان",
+                    Value = "descending"
+                },
+            },
+            DefaultSort = "ascending",
+            Filters = new Dictionary<string, Filter>
+            {
+                {
+                    "price", new Filter
+                    {
+                        Type = FilterType.Slider,
+                        Title = "نرخ",
+                        Icon = "slider-icon",
+                        Options = new SliderFilterOptions
+                        {
+                            Min = 0,
+                            Max = 1000
+                        }
+                    }
+                },
+                {
+                    "productType", new Filter
+                    {
+                        Type = FilterType.Checkbox,
+                        Title = "جۆری بەرهەم",
+                        Icon = "checkbox-icon",
+                        Options = new CheckboxFilterOptions
+                        {
+                            Items = new List<CheckboxOption>
+                            {
+                                new CheckboxOption { Id = "1", Title = "جۆر ١" },
+                                new CheckboxOption { Id = "2", Title = "جۆر ٢" }
+                            }
+                        }
+                    }
+                },
+                {
+                    "duration", new Filter
+                    {
+                        Type = FilterType.Checkbox,
+                        Title = "مەودای کێش",
+                        Icon = "checkbox-icon",
+                        Options = new CheckboxFilterOptions
+                        {
+                            Items = new List<CheckboxOption>
+                            {
+                                new CheckboxOption { Id = "short", Title = "کەم" },
+                                new CheckboxOption { Id = "long", Title = "زۆر" }
+                            }
+                        }
+                    }
+                },
+                {
+                    "material", new Filter
+                    {
+                        Type = FilterType.Checkbox,
+                        Title = "ماددە",
+                        Icon = "checkbox-icon",
+                        Options = new CheckboxFilterOptions
+                        {
+                            Items = new List<CheckboxOption>
+                            {
+                                new CheckboxOption { Id = "metal", Title = "ئاسن" },
+                                new CheckboxOption { Id = "plastic", Title = "پلاستیک" }
+                            }
+                        }
+                    }
+                },
+                {
+                    "occasion", new Filter
+                    {
+                        Type = FilterType.Checkbox,
+                        Title = "بۆنە",
+                        Icon = "checkbox-icon",
+                        Options = new CheckboxFilterOptions
+                        {
+                            Items = new List<CheckboxOption>
+                            {
+                                new CheckboxOption { Id = "wedding", Title = "هاوسەرگیری" },
+                                new CheckboxOption { Id = "daily", Title = "ڕۆژانە" }
+                            }
+                        }
+                    }
+                },
+                {
+                    "braceletType", new Filter
+                    {
+                        Type = FilterType.Checkbox,
+                        Title = "جۆری عەڵقە",
+                        Icon = "checkbox-icon",
+                        Options = new CheckboxFilterOptions
+                        {
+                            Items = new List<CheckboxOption>
+                            {
+                                new CheckboxOption { Id = "leather", Title = "پۆست" },
+                                new CheckboxOption { Id = "chain", Title = "زنجیر" }
+                            }
+                        }
+                    }
+                },
+                {
+                    "buckleType", new Filter
+                    {
+                        Type = FilterType.Checkbox,
+                        Title = "جۆری ملوانکە",
+                        Icon = "checkbox-icon",
+                        Options = new CheckboxFilterOptions
+                        {
+                            Items = new List<CheckboxOption>
+                            {
+                                new CheckboxOption { Id = "hook", Title = "هوک" },
+                                new CheckboxOption { Id = "clasp", Title = "کلیپ" }
+                            }
+                        }
+                    }
+                },
+                {
+                    "bezelType", new Filter
+                    {
+                        Type = FilterType.Checkbox,
+                        Title = "جۆری بازن",
+                        Icon = "checkbox-icon",
+                        Options = new CheckboxFilterOptions
+                        {
+                            Items = new List<CheckboxOption>
+                            {
+                                new CheckboxOption { Id = "fixed", Title = "بەجێماوە" },
+                                new CheckboxOption { Id = "rotating", Title = "بەدوورخستن" }
+                            }
+                        }
+                    }
+                },
+                {
+                    "backType", new Filter
+                    {
+                        Type = FilterType.Checkbox,
+                        Title = "جۆری پشتێن",
+                        Icon = "checkbox-icon",
+                        Options = new CheckboxFilterOptions
+                        {
+                            Items = new List<CheckboxOption>
+                            {
+                                new CheckboxOption { Id = "transparent", Title = "ڕووناک" },
+                                new CheckboxOption { Id = "solid", Title = "تەخت" }
+                            }
+                        }
+                    }
+                },
+                {
+                    "setType", new Filter
+                    {
+                        Type = FilterType.Checkbox,
+                        Title = "جۆری تاقم",
+                        Icon = "checkbox-icon",
+                        Options = new CheckboxFilterOptions
+                        {
+                            Items = new List<CheckboxOption>
+                            {
+                                new CheckboxOption { Id = "single", Title = "تاق" },
+                                new CheckboxOption { Id = "set", Title = "تاقم" }
+                            }
+                        }
+                    }
+                }
+            },
+            Products = new List<ItemDTO>
+            {
+                new ItemDTO
+                {
+                Discount = 2,
+                MainPhoto = "/images/product/0.png",
+                IsFavorite = true,
+                Caption = "گوارەی عەیارە 18",
+                CostPerGram = 10M,
+                Weight = 3.5M,
+                },
+                new ItemDTO
+                {
+                Discount = 3,
+                MainPhoto = "/images/product/2.png",
+                IsFavorite = true,
+                Caption = "گوارەی عەیارە 18",
+                CostPerGram = 10M,
+                Weight = 41.5M,
+                },
+                new ItemDTO
+                {
+                Discount = 15,
+                MainPhoto = "/images/product/1.png",
+                IsFavorite = true,
+                Caption = "گوارەی عەیارە 24",
+               CostPerGram = 25M,
+                Weight = 13.5M,
+                },
+                new ItemDTO
+                {
+                Discount = 5M,
+                MainPhoto = "/images/product/1.png",
+                IsFavorite = true,
+                Caption = "گوارەی عەیارە 24",
+                CostPerGram = 10M,
+                Weight = 23.5M,
+                }
+            }
+        };
+    }
+}
